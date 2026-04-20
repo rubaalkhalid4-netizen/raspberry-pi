e6c950dfb390df916437e09baa891855a67db660
rubaalkhalid4-netizen <rubaalkhalid4@gmail.com>
2026-04-20 13:13:47 +0300

    حدث main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,6 +1,22 @@
+function capture () {
+	
+}
+input.onButtonPressed(Button.A, function () {
+    الاشارة = 1
+})
+input.onButtonPressed(Button.B, function () {
+    الاشارة = 0
+})
+let الاشارة = 0
+for (let index = 0; index < 1; index++) {
+    basic.showNumber(0)
+    الاشارة += 0
+}
 basic.forever(function () {
-    basic.showNumber(0 + 0)
-    if (true) {
+    if (0 == 0) {
+        serial.writeLine("capture")
+        serial.redirectToUSB()
+    } else {
     	
     }
 })
